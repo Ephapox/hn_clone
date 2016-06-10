b93214e902f5aeda5f01caa9cb973d39649f3baf
Samuel Chun <ephapox@gmail.com>
2016-06-09 18:37:51 -0700

    adds public path

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var ENTRY_PATH = path.resolve('./app');
-var BUILD_PATH = path.resolve('./app/build');
-var PUBLIC_PATH = '/public/assets';
+var ENTRY_PATH = path.resolve('app');
+var BUILD_PATH = path.resolve('app/build');
+var PUBLIC_PATH = '/public/assets/';
 
 module.exports = {
 	context: ENTRY_PATH,
@@ -12,6 +12,7 @@ module.exports = {
 	},
 	output: {
 		path: BUILD_PATH,
+		publicPath: PUBLIC_PATH,
 		filename: 'bundle.js'
 	},
 	plugins: [
